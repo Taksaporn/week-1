cb0c8e4c30c37ca4cc87b78e3b2f0334882cb79e
Taksaporn Chaikeawmay <chaikeawmay.t@gmail.com>
2015-08-21 16:45:12 +0700

    add three function

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -10,34 +10,49 @@ namespace Lab1_2
     {
         static void Main(string[] args)
         {
+            BubbleSortFunction  ip = new BubbleSortFunction(); // if not call class is not call function plss remember
+            int[] a = ip.input();
+            ip.process(a);
+            ip.output(a);
+          
+        } 
+        
+        // input
+        public int []input()
+        { 
+             int [] x = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+             return x;
+        }
+
+        public void process(int[] input) {
+            
             bool flag = true;
-
-            // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
             while (flag)
-            {
-                flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    flag = false;
+                    for (int i = 0; i != input.Length - 1; i++)
                     {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
-                        flag = true;
+                        if (input[i] > input[i + 1])
+                        {
+                            int temp = input[i];
+                            input[i] = input[i + 1];
+                            input[i + 1] = temp;
+                            flag = true;
+                        }
                     }
                 }
-            }
+        }
 
-            // output
+        public void output(int[] input)
+        {
+           
             for (int i = 0; i != input.Length; i++)
-            {
-                Console.Write(input[i]);
-                Console.Write(" ");
-            }
-            Console.ReadKey();
+                {
+                    Console.Write(input[i]);
+                    Console.Write(" ");
+                }
+                Console.ReadKey();
+                
         }
     }
 }
